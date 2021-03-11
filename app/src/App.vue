49247df8683975f98d58730c765f537f5f27ea6c
Iago Ferreira <iago-ferreira@outlook.com>
2021-03-11 17:32:08 -0300

    feat: updated App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,97 +1,30 @@
 <template>
   <div>
-    <div>
-      <!-- <input type="text" name="new_task" 
-      :value="todo.text"
-      @input="todo.text = $event.target.value"
-       /> -->
-      <input type="text" name="new_task" v-model="todo.text" />
-      <button @click="addTask">Add</button>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">Todolist</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/tasks">Tasks</b-nav-item>
+          <b-nav-item to="/products">Products</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto"> </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+
+    <div class="container-fluid mt-4">
+      <router-view />
     </div>
-
-    <ol>
-      <li
-        v-for="todo in todos"
-        :key="todo.id"
-        class="task"
-        :class="{ active: todo.done }"
-      >
-        <span>{{ todo.text }}</span>
-        <button type="button" @click="doneTask(todo, $event)">
-          {{ todo.done ? "Concluido" : "Concluir" }}
-        </button>
-        <button type="button" @click="selectToEditTask(todo, $event)">
-          Editar
-        </button>
-        <button type="button" @click="removeTask(todo, $event)">Remover</button>
-      </li>
-    </ol>
-
-    <!--Listando chaves e valores de um objeto 
-  <ul>
-    <li v-for="(value, key) of todo" :key="key">
-      {{ key }} : {{ value }}
-    </li>
-  </ul> -->
   </div>
 </template>
 
 <script>
 export default {
   name: "App",
-  data() {
-    return {
-      todo: {
-        text: "",
-        done: false,
-      },
-
-      todos: [],
-    };
-  },
-
-  methods: {
-    addTask(event) {
-      // $event = JS Event
-      if (this.todo.id) {
-        // Atualizar
-        const index = this.todos.findIndex((t) => t.id === this.todo.id);
-        if (index !== -1) {
-          this.todos.splice(index, 1, { ...this.todo });
-        }
-      } else {
-        // Adicionar
-        const id = this.todos.length
-          ? this.todos[this.todos.length - 1].id + 1
-          : 1;
-        this.todos.push({ ...this.todo, done: false, id });
-      }
-
-      this.todo = {};
-    },
-    doneTask(task, event) {
-      const index = this.todos.findIndex((t) => t.id === task.id);
-      if (index !== -1) {
-        this.todos.splice(index, 1, { ...task, done: !task.done });
-      }
-    },
-    removeTask(task) {
-      const index = this.todos.findIndex((t) => t.id === task.id);
-      if (index !== -1) {
-        this.todos.splice(index, 1);
-      }
-    },
-
-    selectToEditTask(task) {
-      this.todo = { ...task };
-    },
-  },
 };
 </script>
-
-<style lang="scss">
-
-.task.active span {
-  text-decoration: line-through;
-}
-</style>
